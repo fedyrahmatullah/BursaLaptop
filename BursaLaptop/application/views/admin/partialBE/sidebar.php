e131fb559f7db6719e454e54d5a73bb57819fb49
LaficPrayana1398 <lafic.prayana@gmail.com>
2019-04-27 13:04:31 +0700

    Template admin

---
+++ BursaLaptop/application/views/admin/partialBE/sidebar.php
@@ -0,0 +1,50 @@
+<ul class="sidebar navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.html">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Dashboard</span>
+        </a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fas fa-fw fa-folder"></i>
+          <span>Data User</span>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+          <h6 class="dropdown-header">Karyawan</h6>
+          <a class="dropdown-item" href="">Admin</a>
+          <a class="dropdown-item" href="forgot-password.html">Divisi Service</a>
+          <a class="dropdown-item" href="forgot-password.html">Kasir</a>
+          <h6 class="dropdown-header">Mitra</h6>
+          <a class="dropdown-item" href="">Pelanggan</a>
+          <a class="dropdown-item" href="">Suplier</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fas fa-fw fa-cart-plus"></i>
+          <span>Transaksi</span>
+        </a>
+          <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+          <a class="dropdown-item" href="login.html">Pesanan Service masuk</a>
+          <a class="dropdown-item" href="register.html">Pesanan Service <br > dikerjakan</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fas fa-fw fa-table"></i>
+          <span>Laporan</span>
+        </a>
+          <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+          <h6 class="dropdown-header">Laporan Harian</h6>
+          <a class="dropdown-item" href="login.html">Laporan Jasa Service</a>
+          <a class="dropdown-item" href="login.html">Laporan Penjualan <br>Acc dan Sparepart</a>
+          <h6 class="dropdown-header">Laporan Mingguan</h6>
+          <a class="dropdown-item" href="login.html">Laporan Jasa Service</a>
+          <a class="dropdown-item" href="login.html">Laporan Penjualan<br> Acc dan Sparepart</a>
+          <h6 class="dropdown-header">Laporan Bulanan</h6>
+          <a class="dropdown-item" href="login.html">Laporan Jasa Service</a>
+          <a class="dropdown-item" href="login.html">Laporan Penjualan <br> Acc dan Sparepart</a>
+        </div>
+      </li>
+    </ul>
